646db3ea82c25f1661b71cde497f9d7d9905165e
huanyingtian <n18331160171@sina.com>
2016-11-09 20:50:31 +0800

    website

--- js/self.js
+++ js/self.js
@@ -52,73 +52,68 @@ window.onload=function(){
         setInterval(clock,1000);
     })();
     ;(function(){
-        ;(function(){
-            var oUl = document.querySelector('.banner div ul');
-            var oOl = document.querySelector('.banner div ol');
-            var n = 0;
-            var aLi = document.querySelectorAll('.banner div ul li');
-            var aBtn = document.querySelectorAll('.banner div ol li');
-            var timer = null;
-            var oLeft = document.querySelectorAll('.banner div a')[0];
-            var oRight = document.querySelectorAll('.banner div a')[1];
-            var oDiv = document.querySelector('.banner div');
-            for(var i=1;i<aBtn.length;i++){
-                aBtn[i].index = i;
+        var oUl = document.querySelector('.banner div ul');
+        var oOl = document.querySelector('.banner div ol');
+        var n = 0;
+        var aLi = document.querySelectorAll('.banner div ul li');
+        var aBtn = document.querySelectorAll('.banner div ol li');
+        var timer = null;
+        var oLeft = document.querySelectorAll('.banner div a')[0];
+        var oRight = document.querySelectorAll('.banner div a')[1];
+        var oDiv = document.querySelector('.banner div');
+        for(var i=0;i<aBtn.length;i++){
+            aBtn[i].index = i;
 
-                aBtn[i].onclick=function(){
-                    n=this.index;
-                    for(var i=0;i<aBtn.length;i++){
-                        aBtn[i].className = ''
-                    };
-                    aBtn[n].className='active';
-                    oUl.style.transform='translateX('+-(n*aLi[0].offsetWidth)+'px)';
-                }//点击
-            }
-            oDiv.onmouseover=function(){
-                clearInterval(timer);
-                oLeft.style.display='block';
-                oRight.style.display='block';
-                oDiv.onmouseout=function(){
-                    oLeft.style.display='none';
-                    oRight.style.display='none';
-                    timer=setInterval(clock,5000)
-                }
-            }
-            function tab(){
+            aBtn[i].onclick=function(){
+                n=this.index;
                 for(var i=0;i<aBtn.length;i++){
                     aBtn[i].className = ''
                 };
                 aBtn[n].className='active';
-                oUl.style.transform='translateX('+-(n*aLi[0].offsetWidth)+'px)'
+                oUl.style.transform='translateX('+(-n*aLi[0].offsetWidth)+'px)';
+            }//点击
+        }
+        oDiv.onmouseover=function(){
+            clearInterval(timer);
+            oLeft.style.display='block';
+            oRight.style.display='block';
+            oDiv.onmouseout=function(){
+                oLeft.style.display='none';
+                oRight.style.display='none';
+                timer=setInterval(clock,5000)
             }
-            oRight.onclick=function(){
-                n++;
-                if(n>aBtn.length-1){
-                    n=0;
-                }
-                tab();
+        }
+        function tab(){
+            for(var i=0;i<aBtn.length;i++){
+                aBtn[i].className = ''
+            };
+            aBtn[n].className='active';
+            oUl.style.transform='translateX('+-(n*aLi[0].offsetWidth)+'px)'
+        }
+        oRight.onclick=function(){
+            n++;
+            if(n>aBtn.length-1){
+                n=0;
             }
-            oLeft.onclick=function(){
-                n--;
-                if(n<0){
-                    n=aBtn.length-1;
-                }
-                tab();
+            tab();
+        }
+        oLeft.onclick=function(){
+            n--;
+            if(n<0){
+                n=aBtn.length-1;
             }
-            function clock(){
-                n++;
-                if(n>aBtn.length-1){
-                    n=0;
-                }
-                tab();
+            tab();
+        }
+        function clock(){
+            n++;
+            if(n>aBtn.length-1){
+                n=0;
             }
-            clock();
-            timer=setInterval(clock,5000)
-        })();
-        ;(
-            function(){
-
-            }
-        )();
+            tab();
+        }
+        clock();
+        timer=setInterval(clock,5000)
     })();
+    ;(function(){})();
+    ;(function(){})();
 }
