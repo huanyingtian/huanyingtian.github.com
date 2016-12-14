5d05d7cda913e258d0bb32c798b732c8c0091cd9
huanyingtian <n18331160171@sina.com>
2016-12-14 12:03:08 +0800

    website

--- js/website.js
+++ js/website.js
@@ -1,4 +1,4 @@
-function myfun{
+window.onload=function(){
     ;(function(){
         var oClock1 = document.querySelector('.clock1');
         var oH = document.querySelector('.h');
@@ -11,25 +11,34 @@ function myfun{
             oClock1.appendChild(oSpan);
             var oEm = document.createElement('em');
             if(i%5==0){
-                oSpan.style.height='20px';
+                oSpan.style.height='0.5rem';
                 oSpan.style.background='#63FF2D';
                 oEm.style.transform='rotate('+-(i*6)+'deg)';
                 oEm.innerHTML=(i/5||12)
-            }
+            };
             oSpan.appendChild(oEm);
-        }
+        };
         function clock(){
             var oDate = new Date();
             var h = oDate.getHours();
             var m = oDate.getMinutes();
             var s = oDate.getSeconds();
             var ms = oDate.getMilliseconds();
-            oH.style.transform='rotate('+(h/12*30+m/60*30)+'deg)';
+            oH.style.transform='rotate('+(h%12*30+m/60*30)+'deg)';
             oM.style.transform='rotate('+(m*6+s/60*6)+'deg)';
             oS.style.transform='rotate('+(s*6+ms/1000*6)+'deg)';
-        }
+        };
         clock();
-        timer0=setInterval(clock,1);
+        setInterval(clock,1);
     })();
-}
-window.onload=myfun;
+    ;(function(){
+        var oPhone = document.querySelector('.phone a img');
+        var oInformation = document.querySelector('.information');
+        oPhone.onmouseover=function(){
+            oInformation.style.display='block';
+            oPhone.onmouseout=function(){
+                oInformation.style.display='none';
+            }
+        }
+    })();
+};
